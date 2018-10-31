d096411d1ab70f3e0a872ce76859c164c284849e
Thadeu Costa <Thadeu.Costa@vistajet.com>
2018-10-31 14:41:17 -0300

    added more types. made send data to firebase generic.

--- app/src/main/java/com/example/pichau/rumoaomilhao/MainActivity.kt
+++ app/src/main/java/com/example/pichau/rumoaomilhao/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.pichau.rumoaomilhao
 
 import android.annotation.SuppressLint
+import android.location.LocationProvider
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -21,26 +22,26 @@ import java.time.LocalDateTime
 
 class MainActivity : AppCompatActivity() {
 
-    private val myList = arrayListOf("Thadeu", "Jussara", "Maria")
+    private val myList = arrayListOf("Carlos", "Jussara", "Maria")
     private val carList = arrayListOf("Fiat", "Volks Wagen", "Ford", "Nissan")
+    private lateinit var fusedLocationClient: LocationProvider
 
     fun IntRange.random() =
             Random().nextInt((endInclusive + 1) - start) +  start
 
-    fun runFirebase( lib: Liability){
+    private fun runFirebase(obj:Any){
         val database = FirebaseDatabase.getInstance()
         val myRef = database.getReference("message")
         val currentDateTime = LocalDateTime.now()
 
         myRef.setValue("Hello: Today is $currentDateTime")
         val myRef2 = database.getReference("/")
-
-        val key = myRef2.child("Liabilities").push().key
+        val objType = obj.javaClass.name.substringAfterLast('.')
+        val key = myRef2.child(objType).push().key
         if (key != null) {
-            myRef2.child("Liabilities").child(key).setValue(lib)
-            println("PASSOU POR AQUI!!")
+            myRef2.child(objType).child(key).setValue(obj)
         }
-        println("PASSOU POR AQUI!!")
+
     }
     @SuppressLint("ObsoleteSdkInt")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,29 +53,31 @@ class MainActivity : AppCompatActivity() {
         homeBtn.setOnClickListener{
             val choseNumber = Random()
             val randomText = myList[choseNumber.nextInt(myList.count())]
-            val carText = carList[choseNumber.nextInt(carList.count())]
             person.name = randomText
             if(randomText == "Thadeu"){
                 homeImage.setImageResource(R.drawable.business_man)
             }
-            val car = PrivateCar(carText, choseNumber.nextInt(500))
-            car.maker = "fabricante"
-            car.year = Year.parse((1950..2020).random().toString())
-            car.model = "modelo"
+            var car:PrivateCar
+            for(i in 0..3){
+                val carText = carList[choseNumber.nextInt(carList.count())]
+                car = PrivateCar(carText, choseNumber.nextInt(500))
+                car.maker = "fabricante - $i"
+                car.year = Year.parse((1950..2020).random().toString())
+                car.model = "modelo - $i"
+                person.liabilities.add(car)
+                runFirebase(car)
+            }
 
-            person.liabilities.add(car)
             profileNameTxt.text = person.name
             val df = DecimalFormat("#.##")
             df.roundingMode = RoundingMode.CEILING
             totalBalanceTxt.text = df.format(choseNumber.nextDouble()*10000)
 
             println("You Clicked in Me $randomText!")
-            println("car = ${car}")
-
             val valueTV = TextView(this)
             valueTV.text = randomText
             investmentsLinearLayout.addView(valueTV)
-            runFirebase(car)
+            runFirebase(person)
         }
     }
 
--- app/src/main/java/com/example/pichau/rumoaomilhao/finance/PrivateCar.kt
+++ app/src/main/java/com/example/pichau/rumoaomilhao/finance/PrivateCar.kt
@@ -3,10 +3,11 @@ package com.example.pichau.rumoaomilhao.finance
 import java.time.Year
 
 
-class PrivateCar(var name: String, id: Int) : Liability(name, id, "Car") {
+class PrivateCar(var name: String, id: Int) : Liability(name, id) {
     var maker: String = ""
     var model: String = ""
     var year: Year = Year.parse("1900")
+    var currentValue : Number? = 0
+    var maintenanceCosts : Number? = 0
 
-    override fun toString(): String = "name = $name\nmodel = $model\nyear = ${year}\nmaker = $maker"
 }
